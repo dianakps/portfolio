10c929169226e32428ada753c565067744c21491
dianakps <dianakps@live.com>
2024-11-01 23:45:23 +0200

    Deploy Next.js to GitHub Pages

---
+++ _next/static/chunks/pages/index-ba76d6390a8aa944.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5557:(_,n,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(4103)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[888,774,179],()=>n(5557)),_N_E=_.O()}]);
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+/**
+ * @type {import('next').NextConfig}
+ */
+// Correct for ES module
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
