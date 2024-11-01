78ca88a319deb449fd075e84760717fcc7992191
dianakps <dianakps@live.com>
2024-11-02 00:02:20 +0200

    Deploy static site to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,14 @@
-/** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
-
-// export default nextConfig;
-
 /**
  * @type {import('next').NextConfig}
  */
-// Correct for ES module
+
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
 };
 
 export default nextConfig;
