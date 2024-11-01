8382508a188e9c56fb2e33423ffe72696db1d352
dianakps <dianakps@live.com>
2024-11-01 23:17:18 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import Home from "./index";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Home />
+    </>
+  );
+
+  // <Component {...pageProps} />;
 }
